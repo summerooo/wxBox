cb3bf3b1d9439cb76ec152b580e536f6e1bdaf22
WrenchDE <1695408430@qq.com>
2019-02-18 15:39:06 +0800

    upedate

--- src/views/buyGoods.vue
+++ src/views/buyGoods.vue
@@ -441,9 +441,9 @@ export default {
         goods_info.push({goods_code: this.cart[i].goods_code, goods_number: this.cart[i].goods_number, goods_name: this.cart[i].goods_name})
       }
       console.log(goods_info)
-      let br = await prepayWeixinOrder({ token: JSON.parse(authorityToken), goods_info: goods_info, box_no: this.box_no, order_source: 4, original_price: 0, preferential_amount: 0, payable_fee: 0, preferential_type: 0, discount_id: 0, user_coupon_id: 0 })
+      let br = await prepayWeixinOrder({ token: JSON.parse(authorityToken).token, goods_info: goods_info, box_no: this.box_no, order_source: 4, original_price: 0, preferential_amount: 0, payable_fee: 0, preferential_type: 0, discount_id: 0, user_coupon_id: 0 })
       console.log(br, 'prepayWeixinOrderprepayWeixinOrder')
-      let wxpso = await weixinPaySaleOrder(Object.assign({token: JSON.parse(authorityToken), order_origin: 4, channel: 1}, br.data.return_data))
+      let wxpso = await weixinPaySaleOrder(Object.assign({token: JSON.parse(authorityToken).token, order_origin: 4, channel: 1}, br.data.return_data))
       let that = this
       wx.config(JSON.parse(sessionStorage.getItem('buyAuthority')))
       wx.ready(() => {
